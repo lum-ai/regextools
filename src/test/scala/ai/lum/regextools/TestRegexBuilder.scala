f3fb27c2a520b0d5dc98658018786f4a3b435957
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2019-02-08 22:54:07 -0700

    tests

--- src/test/scala/ai/lum/regextools/TestRegexBuilder.scala
+++ src/test/scala/ai/lum/regextools/TestRegexBuilder.scala
@@ -14,4 +14,22 @@ class TestRegexBuilder extends FlatSpec with Matchers {
     builder.mkPattern shouldEqual "foo(?:[bx]ar|zap)"
   }
 
+  it should "use the optional operator" in {
+    val builder = new RegexBuilder
+    builder.add("foobar")
+    builder.mkPattern shouldEqual "foobar"
+    builder.add("fooar")
+    builder.mkPattern shouldEqual "foob?ar"
+  }
+
+  it should "build pattern from urls" in {
+    val builder = new RegexBuilder
+    builder.add("appserver1.domain.tld")
+    builder.mkPattern shouldEqual "appserver1\\.domain\\.tld"
+    builder.add("appserver2.domain.tld")
+    builder.mkPattern shouldEqual "appserver[12]\\.domain\\.tld"
+    builder.add("appserver3.domain.tld")
+    builder.mkPattern shouldEqual "appserver[123]\\.domain\\.tld"
+  }
+
 }
