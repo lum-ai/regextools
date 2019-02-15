00d4e5e8e1087fc0a8ed875bb856cf757a0e2ec8
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2019-02-14 22:51:38 -0700

    changes

--- src/main/scala/ai/lum/regextools/RegexUtils.scala
+++ src/main/scala/ai/lum/regextools/RegexUtils.scala
@@ -31,19 +31,24 @@ object RegexUtils {
     s.map(c => if (metacharacters contains c) s"\\$c" else c).mkString
   }
 
-  // pairs: "(){}[]<>"
+  /** Makes regexes for bracketed strings.
+   *  pairs: "(){}[]<>"
+   */
   def mkCharBracketed(pairs: String, escapes: String = "\\"): String = {
     implicit def asPair(a: Array[String]): (String, String) = (a(0), a(1))
-    require(pairs.nonEmpty, "missing pairs")
-    require(pairs.size % 2 == 0, "incomplete pair")
+    if (pairs.isEmpty) throw new IllegalArgumentException("missing pairs")
+    if (pairs.size % 2 != 0) throw new IllegalArgumentException("incomplete pair")
     val (open, close) = pairs.split("").grouped(2).toArray.unzip
-    mkCharDelimited(open.mkString, close.mkString, escapes)
+    mkCharDelimited(open.mkString, escapes, close.mkString)
   }
 
+  /** Makes regexes for delimited strings.
+   *  Uses non-capturing groups and possessive quantifiers for efficiency.
+   */
   def mkCharDelimited(
       delimiters: String,
-      closeDelimiters: String = "",
-      escapes: String = "\\"
+      escapes: String = "\\",
+      closeDelimiters: String = ""
   ): String = {
     require(delimiters.nonEmpty, "missing delimirers")
     val numDelimiters = delimiters.size
@@ -52,13 +57,13 @@ object RegexUtils {
       case 0 => "" // no escape char
       case 1 => escapes * numDelimiters // same escape char for all delimiters
       case `numDelimiters` => escapes // distinct escape char per delimiter
-      case _ => throw new Exception
+      case _ => throw new IllegalArgumentException("invalid escapes")
     }
     // get close delimiters
     val cdels = closeDelimiters.size match {
       case 0 => delimiters // close delimiters default to open delimiters
       case `numDelimiters` => closeDelimiters // distinct close delimiter per open delimiter
-      case _ => throw new Exception
+      case _ => throw new IllegalArgumentException("illegal close delimiters")
     }
     // get patterns for each open/close delimiter pair
     val patterns = for (i <- delimiters.indices) yield {
@@ -69,13 +74,13 @@ object RegexUtils {
       // make pattern
       if (cdel == esc) {
         // escape is equal to close delimiter
-        s"$del[^$cdel]*(?:$cdel$cdel[^$cdel]*)*$cdel"
+        s"$del[^$cdel]*+(?:$cdel$cdel[^$cdel]*+)*+$cdel"
       } else if (esc.nonEmpty) {
         // escape char can be used on to escape any char
-        s"$del[^$esc$cdel]*(?:$esc.[^$esc$cdel]*)*$cdel"
+        s"$del[^$esc$cdel]*+(?:$esc.[^$esc$cdel]*+)*+$cdel"
       } else {
         // no escape char
-        s"$del[^$cdel]*$cdel"
+        s"$del[^$cdel]*+$cdel"
       }
     }
     // return single regex for all patterns
